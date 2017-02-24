cb1cb9bcc4d5c44c70ec3ebf38e3a22e43096a87
Ben <bensass123@gmail.com>
2017-02-24 11:53:24 -0500

    3

--- flash.js
+++ flash.js
@@ -1,6 +1,8 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 var response, response2;
+var basicArray = [];
+var clozeArray = [];
 
 
 class Basic {
@@ -19,18 +21,104 @@ class Basic {
 
 class Cloze {
   constructor(text, cloze) {
-    this.text = text;
-    this.cloze = cloze;
+  	if (text.includes(cloze)){
+  		console.log('creating');
+	    this.text = text;
+	    this.cloze = cloze;
+	}
+	else {
+		return null;
+	}
   }
 
-  cloze() {
-  	return this.cloze;
+  get partial() {
+  	//fix this
+  	return this.text.replace(this.cloze,'...');
   }
 
-  text() {
+  get fullText() {
   	return this.text;
   }
 }
+
+
+var basicFile = fs.readFileSync('basic.txt').toString();
+var clozeFile = fs.readFileSync('cloze.txt').toString();
+
+var basicArray = basicFile.split('BASIC');
+var clozeArray = clozeFile.split('CLOZE');
+basicArray.splice(0,1);
+clozeArray.splice(0,1);
+
+console.log('clozeArray length: ' + clozeArray.length);
+
+// var front = b.substring(b.lastIndexOf("FRONT --- ")+10,b.lastIndexOf(" --- BACK"));
+
+
+var basicFrontArray = basicArray.map(function(b){
+	return b.substring(b.lastIndexOf("FRONT --- ")+10,b.lastIndexOf(" --- BACK"));
+  });
+
+console.log('\r\n Basic Fronts:')
+for (i in basicFrontArray) {
+	console.log(basicFrontArray[i]);
+}
+
+var clozeFrontArray = clozeArray.map(function(b){
+	return b.substring(b.lastIndexOf("FRONT --- ")+10,b.lastIndexOf(" --- BACK"));
+  });
+
+console.log('\r\n Cloze Fronts:')
+for (i in clozeFrontArray) {
+	console.log(clozeFrontArray[i]);
+}
+
+var basicBackArray = basicArray.map(function(b){
+	return b.substring(b.lastIndexOf("BACK --- ")+9, b.length).trim();
+  });
+
+console.log('\r\n Basic Backs:')
+for (i in basicBackArray) {
+	console.log(basicBackArray[i]);
+}
+
+var clozeBackArray = clozeArray.map(function(b){
+	return b.substring(b.lastIndexOf("BACK --- ")+9, b.length).trim();
+  });
+
+console.log('\r\n Cloze Backs:')
+for (i in clozeBackArray) {
+	console.log(clozeBackArray[i]);
+}
+
+
+//create array of objects
+var clozeCards = [];
+var basicCards = [];
+
+for (i in basicFrontArray){
+	var a = new Basic(basicFrontArray[i], basicBackArray[i]);
+	basicCards.push(a);
+}
+
+for (i in clozeFrontArray){
+	var a = new Cloze(clozeFrontArray[i], clozeBackArray[i]);
+	clozeCards.push(a);
+}
+
+
+
+
+console.log('Cloze questions');
+for (i in clozeCards) {
+	console.log(clozeCards[i].partial);
+}
+
+console.log('Cloze full texts');
+for (i in clozeCards) {
+	console.log(clozeCards[i].fullText);
+}
+
 
 inquirer.prompt([
   {
@@ -41,12 +129,12 @@ inquirer.prompt([
   },
   {
     type: "input",
-    message: "Please enter front of flashcard (use ... to indicate blanks in question if cloze, if Basic enter question.",
+    message: "Please enter full answer of flashcard.",
     name: "one"
   },
   {
   	type: "input",
-    message: "Please enter the answer. (If Basic then enter answer, If Cloze enter correct term to fill in the blank.",
+    message: "Please enter the answer. (If Cloze enter correct term to fill in the blank.)",
     name: "two"
   }	
 
@@ -56,18 +144,16 @@ inquirer.prompt([
 	}
   // initializes the variable newguy to be a programmer object which will take
   // in all of the user's answers to the questions above
-  if (card.type === 'Cloze') {
-  	if(card.one.includes('...')) {
-	  	response = `CLOZE --- FRONT --- ${card.one} --- BACK --- ${card.two} \r\n\r\n`;
-	  	fs.appendFile('cloze.txt', response, function (err) {
-	  		if (err) {
-	  			console.log(err);
-	  		}
-		});
-	 }
-	 else {
-	 	console.log('Error: You did not include ... in your front of Flashcard entry. Your flashcard was not stored.');
-	 }
+
+  var c = new Cloze(card.one, card.two);
+  if (card.type === 'Cloze' && c) {
+      response = `CLOZE --- FRONT --- ${card.one} --- BACK --- ${card.two} \r\n\r\n`;
+      fs.appendFile('cloze.txt', response, function(err) {
+          if (err) {
+              console.log(err);
+          }
+      });
+
   	console.log('Cloze');
   	console.log(`${card.one} --- ${card.two}`);
   }
